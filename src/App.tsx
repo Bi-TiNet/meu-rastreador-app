75f02193c59d02eea7db54b0db34e6d936e8bff9
Ti.Net Tecnologia <bi@tinettecnologia.com.br>
2025-08-11 11:09:07 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-// src/App.tsx
+// Arquivo: src/App.tsx
+import './App.css'; // Importa o nosso novo estilo global
 import { InstallationForm } from './components/InstallationForm'
 import { Dashboard } from './components/Dashboard'
 
 function App() {
   return (
     <main>
-      {/* O formulário continua aqui em cima */}
       <InstallationForm />
-
-      {/* Adicionamos uma linha divisória e o novo Dashboard embaixo */}
-      <hr style={{ maxWidth: '900px', margin: '2rem auto' }} />
       <Dashboard />
     </main>
   )
