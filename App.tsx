77337349f7897952f053ab7c4036771b41b25699
Ti.Net Tecnologia <bi@tinettecnologia.com.br>
2025-08-11 09:05:22 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 // src/App.tsx
 import { InstallationForm } from './components/InstallationForm'
-import { Dashboard } from './components/Dashboard'
+import { Dashboard } from './components/Dashboard' // Adicionamos a importação do Dashboard
 
 function App() {
   return (
     <main>
+      {/* O formulário continua aqui em cima */}
       <InstallationForm />
+
+      {/* Adicionamos uma linha divisória e o novo Dashboard embaixo */}
       <hr style={{ maxWidth: '900px', margin: '2rem auto' }} />
       <Dashboard />
     </main>
