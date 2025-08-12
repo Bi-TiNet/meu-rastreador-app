5ea39387c89648420401e7d343b5fe9427955f0f
Ti.Net Tecnologia <bi@tinettecnologia.com.br>
2025-08-12 08:26:41 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,22 @@ import { Dashboard } from './components/Dashboard';
 import { TechnicianAgenda } from './components/TechnicianAgenda';
 import { InsuranceView } from './components/InsuranceView';
 
-// Página do Administrador
-function AdminPage() {
-  return (
-    <>
-      <InstallationForm />
-      <Dashboard />
-    </>
-  );
-}
-
 function App() {
   return (
     <BrowserRouter>
+      {/* O menu de navegação foi atualizado para as novas páginas */}
       <nav className="main-nav">
-        <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Painel Administrador</NavLink>
+        <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Cadastrar Instalação</NavLink>
+        <NavLink to="/painel" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Painel de Agendamentos</NavLink>
         <NavLink to="/agenda" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Agenda do Técnico</NavLink>
         <NavLink to="/consulta" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Consulta Seguradora</NavLink>
       </nav>
       
       <main>
+        {/* As rotas foram separadas */}
         <Routes>
-          <Route path="/" element={<AdminPage />} />
+          <Route path="/" element={<InstallationForm />} />
+          <Route path="/painel" element={<Dashboard />} />
           <Route path="/agenda" element={<TechnicianAgenda />} />
           <Route path="/consulta" element={<InsuranceView />} />
         </Routes>
