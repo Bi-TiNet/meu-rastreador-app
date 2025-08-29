5afe635141dfae0842d84abe1482938042d1e41c
carlos <bi@tinettecnologia.com.br>
2025-08-29 08:56:56 -0300

    App Celular

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
+// Arquivo: vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa' // 1. Importe o plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // 2. Adicione a configuração do PWA
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // manifest.json
+      manifest: {
+        name: 'Agenda Autocontrol',
+        short_name: 'Autocontrol',
+        description: 'Sistema de Agendamento de Instalações',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo-icon-192.png', // Caminho para o ícone 192x192 na pasta 'public'
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo-icon-512.png', // Caminho para o ícone 512x512 na pasta 'public'
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
